1c8f4e540dbd568a6588d57486a146180b6881f8
Andrew Scott <as243879@falmouth.ac.uk>
2024-01-23 11:16:05 +0000

    Initial Commit

---
+++ Assets/Characters/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 moveSpeed = new Vector2(1, 1);
+    public Object projectile;
+
+    public float shootCooldown = 0.8f;
+    float cooldownT = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldownT += Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Move(new Vector3(0, moveSpeed.y * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Move(new Vector3(0, -moveSpeed.y * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Move(new Vector3(-moveSpeed.x * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Move(new Vector3(moveSpeed.x * Time.deltaTime, 0, 0));
+        }
+
+        if (Input.GetMouseButton(0) && cooldownT >= shootCooldown)
+        {
+            Shoot(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
+    }
+
+    void Move(Vector3 move)
+    {
+        transform.position += move;
+    }
+
+    void Shoot(Vector2 mousePos)
+    {
+        cooldownT = 0;
+
+        GameObject projectileGO = Instantiate(projectile, transform.position, new Quaternion(0, 0, 0, 0)) as GameObject;
+        Projectile projectileScript = projectileGO.GetComponent<Projectile>();
+        projectileScript.Shoot(mousePos, this.gameObject);
+    }
+}
---
+++ Assets/Projectiles/Projectile.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 10f;
+    public LayerMask hitLayerMask;
+
+    bool moving = false;
+    Vector3 dir;
+    GameObject caster;
+
+    public void Shoot(Vector2 mousePos, GameObject caster)
+    {
+        this.caster = caster;
+
+        Vector3 mousePos3D = new Vector3(mousePos.x, mousePos.y, 0);
+        Vector3 direction = mousePos3D - transform.position;
+        direction.z = 0;
+        dir = direction;
+
+        moving = true;
+    }
+
+    private void Update()
+    {
+        if (!moving)
+            return;
+
+        transform.position += dir * Time.deltaTime * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (hitLayerMask != (hitLayerMask | (1 << other.gameObject.layer)))
+            return;
+
+        if (other.gameObject != caster)
+        {
+            Debug.Log("Hit " + other.name);
+            Destroy(this.gameObject);
+        }
+    }
+}
